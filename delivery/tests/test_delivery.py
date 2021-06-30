c7f535c390a3d3c0686116b08caede3feed7a313
Jialin Yang <yangjialin94@gmail.com>
2021-06-30 14:00:32 -0500

    added test case

--- delivery/tests/test_delivery.py
+++ delivery/tests/test_delivery.py
@@ -6,3 +6,4 @@ client = TestClient(app)
 def test_list_deliveries():
     response = client.get("/deliveries")
     assert response.status_code == 200
+    assert response.json()["deliveries_count"] == len(response.json()["deliveries"])
